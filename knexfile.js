3a012fee81c0d48b82f3ed4f0d25932060dc7afa
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-01-23 11:48:11 -0500

    migrate recipe-book

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,37 @@
-// Update with your config settings.
-
 module.exports = {
-
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
+      filename: "./data/recipe-book.db3"
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      directory: "./data/migrations"
     },
+    useNullAsDefault: true,
     pool: {
-      min: 2,
-      max: 10
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
+      }
     },
-    migrations: {
-      tableName: 'knex_migrations'
+    seeds: {
+      directory: "./data/seeds"
     }
+
+    // production: {
+    //   client: 'postgresql',
+    //   connection: {
+    //     database: 'my_db',
+    //     user:     'username',
+    //     password: 'password'
+    //   },
+    //   pool: {
+    //     min: 2,
+    //     max: 10
+    //   },
+    //   migrations: {
+    //     tableName: 'knex_migrations'
+    //   }
+    // }
   }
-
 };
